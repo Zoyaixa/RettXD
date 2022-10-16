03cc065c2337c817986b254b5e33d2d5ad094e2c
Zoyaixa <115364271+Zoyaixa@users.noreply.github.com>
2022-10-16 20:15:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6283851929130']
+global.ownernomer = "6283851929130"
+global.premium = ['6283851929130']
+global.packname = 'punya MARGARETA'
+global.author = '⁉️'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
