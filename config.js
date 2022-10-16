91518f67247f9afd2b48acd1389d1e0090574d45
Zoyaixa <115364271+Zoyaixa@users.noreply.github.com>
2022-10-16 20:17:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 25
+    free: 8
 }
 global.thumb = fs.readFileSync('./image/naze.jpg')
 global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
